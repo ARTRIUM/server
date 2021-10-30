ccf4aea3cb860b437572f2c9b7bd4dbbe8212b92
heohyeonjin <hyeonjin4870@naver.com>
2021-10-30 14:25:21 +0900

    FEAT: Room API Controller

--- src/main/java/com/example/hanium/room/repository/RoomRepository.java
+++ src/main/java/com/example/hanium/room/repository/RoomRepository.java
@@ -3,6 +3,7 @@ package com.example.hanium.room.repository;
 import com.example.hanium.Auth.model.User;
 import com.example.hanium.room.model.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
